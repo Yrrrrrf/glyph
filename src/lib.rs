7c23d782888554c073781a49504a74b7b495a94e
Fernando Bryan Reza Campos <fer.rezac@outlook.com>
2025-11-04 23:35:27 -0600

    Add wasm-bindgen

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,31 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+
+    // Import JavaScript's console.log function
+    // #[wasm_bindgen(js_namespace = console)]
+    // fn log(s: &str);
+
+    fn alert(s: &str);
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
 }
+
+// pub fn add(left: u64, right: u64) -> u64 {
+//     left + right
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
