c7a55f5d396a8989c24bb8f7f3764b2e878cc61f
Fernando Bryan Reza Campos <fer.rezac@outlook.com>
2025-11-07 00:05:20 -0600

    Update .gitignore and clean up lib.rs; add comment to index.ts

--- src/lib.rs
+++ src/lib.rs
@@ -14,18 +14,3 @@ extern "C" {
 pub fn greet(name: &str) {
     alert(&format!("Hello, {}!", name));
 }
-
-// pub fn add(left: u64, right: u64) -> u64 {
-//     left + right
-// }
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-
-//     #[test]
-//     fn it_works() {
-//         let result = add(2, 2);
-//         assert_eq!(result, 4);
-//     }
-// }
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,6 @@
 import { initWasm } from "./wasm";
 
+// Initializes the Glyph library.
 export async function init(): Promise<void> {
   console.log("Welcome to Glyph");
   await initWasm();
